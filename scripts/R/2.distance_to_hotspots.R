1063faa66092845cbd100dbd56ee82805dc1ca44
Raahil Madhok <raahil_madhok@hks.harvard.edu>
2025-02-28 09:57:59 -0600

    reorganized

---
+++ scripts/R/2.distance_to_hotspots.R
@@ -0,0 +1,76 @@
+# PROJECT: RUM Model
+# PURPOSE: Distance to Observed hotspots
+
+### SET-UP
+# Directories
+rm(list=ls())
+setwd('/Users/rmadhok/Dropbox/biodiversity-wtp/data/')
+
+# Load Packages
+packages <- c('sf', 'tidyverse', 'units')
+pacman::p_load(packages, character.only = TRUE, install = FALSE)
+#require(gmapsdistance)
+
+# Set great circle radius (for google map)
+# radius <- 30
+
+#-----------------------------------------
+# Select observed hotspots
+#-----------------------------------------
+# We identify the straight-line distance 
+# from home to every hotspot visited
+
+# Read ebird trips
+ebird <- readRDS('./intermediate/ebird/ebird_trip_clean.rds')
+
+# Select trips to hotspots (n = 1,347,758 hotspot trips; n =30,327 users)
+ebird <- ebird %>%
+  filter(locality_type == 'H') %>%
+  select(-c(locality_type, locality))
+
+# Straight-line dist from home to hotspot
+ebird$geo_dist <- st_distance(st_as_sf(ebird, 
+                                       coords = c('lon_home', 'lat_home'),
+                                       crs=4326), 
+                              st_as_sf(ebird, 
+                                       coords = c('lon', 'lat'),
+                                       crs=4326), 
+                              by_element = T) %>% set_units(km)
+ebird$geo_dist <- as.numeric(ebird$geo_dist)
+
+# Save observed choice
+saveRDS(ebird, './intermediate/ebird/ebird_trip_hotspots.rds')
+
+#--------------------------------------
+# Driving Distance To Destination
+#--------------------------------------
+
+# Keep trips w/n radius to reduce API calls
+#ebird <- filter(ebird, geo_dist <= radius)
+api_key = rio::import("../data/api-key-ebird-raahil.txt")
+# set.api.key(api_key)
+# map_by_slice <- function(i){
+#   print(i)
+#   this_slice <- tryCatch({
+#     as.data.frame(gmapsdistance(origin = paste(ebird[i,'lat_home'], ebird[i,'lon_home'], sep='+'),
+#                                 destination = paste(ebird[i,'lat'], ebird[i,'lon'], sep='+'),
+#                                 mode='driving'))
+#   }, error = function(e) {
+#     message("Error occurred while mapping ", i, ".")
+#     data.frame(Time = NA, Distance = NA, Status = NA)
+#   })
+#   
+#   return(this_slice)
+# }
+# idx <- 1:nrow(ebird)
+# results <- lapply(idx, map_by_slice)
+# results <- as.data.frame(rbindlist(results))
+# ebird <- cbind(ebird, results[,c('Time', 'Distance')])
+# ebird <- ebird %>%
+#   rename(drive_time = Time, drive_dist = Distance) %>%
+#   mutate(drive_time = drive_time / 60, 
+#          drive_dist = drive_dist / 1000)
+# rm(list='results')
+
+# Save observed choice
+#saveRDS(ebird, 'ebird_trip_hotspots.rds')
---
+++ scripts/R/functions/hotspot_clustering.R
@@ -0,0 +1,88 @@
+# PURPOSE: Create clusters from hotspots
+# AUTHOR: Raahil Madhok
+
+# Load Packages
+require(cluster)    # clustering algorithms
+require(factoextra)
+
+rec_clust <- function(df, 
+                      module = 'all',
+                      clust_size = 10){
+  
+  # Function: rec_clust
+  ## Inputs: 
+  ### df: a data frame - hotspots
+  ### clust_size: cluster size (km)
+  
+  ## Outputs:
+  ## df: dataframe of hotspots with rec area ID's
+  
+  # Load district map
+  india_dist <- st_read('./data/shp/district-2011/district-2011.shp')
+  
+  
+  # add district code to hostpots
+  df$c_code_2011 <- st_join(st_as_sf(df, coords=c('lon', 'lat'), crs=4326), 
+                            india_dist, join = st_intersects)$c_code_11
+  
+  # Remove off-coast hotspots
+  df <- filter(df, !is.na(c_code_2011))
+  
+  # Drop duplicates
+  #---------------------------------------------------
+  # Some hotspots w same lat-lon entered twice
+  # Some hotspots have same name, coords slightly off
+  #----------------------------------------------------
+  df <- distinct(df, lat, lon, .keep_all=T) # drop duplicate coordinates 
+  df <- distinct(df, c_code_2011, name, .keep_all=T) # drop duplicate names w/n district
+  
+  #------------------------------------------------------
+  # Heirarchical Clustering of Hotspots
+  #------------------------------------------------------
+  
+  # Distance Matrix (5 mins)
+  print('computing distance matrix between hotspots...')
+  distm <- st_distance(st_as_sf(df, coords = c('lon', 'lat'), crs=4326))
+  
+  # Construct heirachical dendogram
+  hc <- hclust(as.dist(distm), method="complete")
+  
+  # Cut hc tree at specified distance (in meters)
+  # Assigns hotspot cluster id to each hotspot
+  df$hsid <- cutree(hc, h=clust_size*1000)
+  
+  if(module == 'all') {
+    
+    # Save intermediate
+    df <- select(df, -hsid)
+    saveRDS(df, paste('./data/intermediate/hotspots/hotspots_all_', clust_size, 'km.rds', sep=''))
+    
+    return(df)
+  
+  }
+
+  if(module == 'cluster'){
+    
+    # Cluster centroids
+    df <- df %>%
+      group_by(hsid) %>%
+      summarize(lat = mean(lat), lon = mean(lon))
+    
+    # District in centroid of recreation area
+    df$c_code_2011 <- st_join(st_as_sf(df, 
+                                       coords=c('lon', 'lat'), 
+                                       crs=4326), 
+                              india_dist, 
+                              join = st_intersects)$c_code_11
+    
+    # remove clusters with off-coast centers
+    df <- filter(df, !is.na(c_code_2011))
+    
+    # Save intermediate data
+    writeRDS(df, paste('./data/intermediate/hotspots/hotspots_clust_', clust_size, 'km.rds', sep=''))
+   
+    return(df)
+  
+    }
+  
+}
